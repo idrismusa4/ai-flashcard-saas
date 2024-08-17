72b3c999e39cc19536a36ec8e3c61434697a2688
Harrison Akunne <harrisonderick.65@gmail.com>
2024-08-17 17:28:24 +0100

    chore: updated stripe apiVersion to latest

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -7,7 +7,7 @@ const formatAmountForStripe = (amount, currency) => {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2024-06-20",
 });
 
 //POST Function
--- app/api/stripe_webhooks/route.js
+++ app/api/stripe_webhooks/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2024-06-20",
 });
 
 export async function POST(request) {
